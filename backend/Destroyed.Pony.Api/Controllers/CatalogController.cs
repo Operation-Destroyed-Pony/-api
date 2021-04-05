c204e5697c76f538b6bbfb5ba0bf1e250bf21bdd
Chungeun Cho <chungeun.cho@gmail.com>
2021-04-05 12:45:59 -0400

    commit on final version

--- backend/Destroyed.Pony.Api/Controllers/CatalogController.cs
+++ backend/Destroyed.Pony.Api/Controllers/CatalogController.cs
@@ -34,6 +34,26 @@ namespace Destroyed.Pony.Api.Controllers
             return Created("/catalog/42", item);
         }
 
+        [HttpPost("{id:int}/ratings")]
+        public IActionResult PostRating(int id, [FromBody] Rating rating)
+        {
+            var item = new Item("Shirt", "Ohio State shirt", "Nike", 29.99m);
+            item.ID = id;
+            item.AddRating(rating);
+
+            return Ok(item);
+        }
+
+        [HttpPut("{id:int}")]
+        public IActionResult PutItem(int id, [FromBody] Item item) {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteItem(int id) {
+            return Ok();
+        }
+
     }
 
 }
