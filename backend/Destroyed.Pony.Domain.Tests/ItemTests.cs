7971c11934c759742a2bb3bea5db441392ae49d5
Chungeun Cho <chungeun.cho@gmail.com>
2021-04-26 11:33:14 -0400

    commit on main

--- backend/Destroyed.Pony.Domain.Tests/ItemTests.cs
+++ backend/Destroyed.Pony.Domain.Tests/ItemTests.cs
@@ -1,12 +1,38 @@
 using Destroyed.Pony.Domain.Tests.RatingTests.cs;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-[TestMethod]
-public void Can_Create_New_Item()
+namespace Destroyed.Pony.Domain.Tests
 {
-	var item = new Item(“Name”, “Description”, “Brand”, 10.00m);
+    [TestClass]
+    public class RatingTests
+    {
+        [TestMethod]
+        public void Can_Create_New_Item()
+        {
+            var item = new Item("Name", "Description", "Brand", 10.00m);
 
-	Assert.AreEqual(“Name”, item.Name);
-	Assert.AreEqual(“Description”, item.Description);
-	Assert.AreEqual(“Brand”, item.Brand);
-	Assert.AreEqual(10.00m, item.Price);
+            Assert.AreEqual("Name", item.Name);
+            Assert.AreEqual("Description", item.Description);
+            Assert.AreEqual("Brand", item.Brand);
+            Assert.AreEqual(10.00m, item.Price);
+        }
+
+        [TestMethod]
+        public void Can_Create_Add_Rating()
+        {
+
+            // Arrange
+            var item = new Item("Name", "Description", "Brand", 10.00m);
+            var rating = new Rating(5, "Name", "Review");
+
+            //Act
+            item.AddRating(rating);
+
+            //Assert
+            Assert.AreEqual(rating, item.Ratings[0]);
+        }
+
+
+    }
+
 }
