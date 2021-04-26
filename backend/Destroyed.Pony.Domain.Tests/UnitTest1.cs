ee6bb185c7a7276747cb8e8d70738a827e406e8b
Chungeun Cho <chungeun.cho@gmail.com>
2021-04-26 11:10:13 -0400

    commit on part 1

---
+++ backend/Destroyed.Pony.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Destroyed.Pony.Domain.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
